01199f528ad94d99a36ccc1054cceafa19829521
Sidney <slack.hiroshi@gmail.com>
2014-11-23 23:19:29 -0200

    testes

--- hello.py
+++ hello.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/messages', methods = ['POST'])
+def api_message():
+    return "JSON Message: " + json.dumps(request.json)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
