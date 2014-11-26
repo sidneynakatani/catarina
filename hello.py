ae9ae3c5da5ce870da7f385a46aa4e7e48847b32
koding user <sidney.nakatani@hotmail.com>
2014-11-26 21:44:31 +0000

    Import connection class

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from flask import Flask,json,request
+from db.connectionfactory import ConnectionFactory
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/test')
+def test():
+    return ConnectionFactory.create()
 
 @app.route('/messages', methods = ['POST'])
 def api_message():
