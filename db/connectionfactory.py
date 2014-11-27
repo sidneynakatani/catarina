4396f8d82447db83c6b17b3aea778b30d49b7ea8
koding user <sidney.nakatani@hotmail.com>
2014-11-27 17:36:48 +0000

    ajustes

--- db/connectionfactory.py
+++ db/connectionfactory.py
@@ -1,8 +1,17 @@
 import psycopg2
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Create app
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tdvtpvxbzvxrqc:GpTck_d1IKA2tv3lu-7JZEMR0F@ec2-54-197-241-82.compute-1.amazonaws.com/dakr17pq1c5236'
+
+# Create database connection object
+db = SQLAlchemy(app)
 
 
 class ConnectionFactory:
-
 
 
     def create(self):
