f6bd68282bc15b7490af82c7bfbe2761432ea913
koding user <sidney.nakatani@hotmail.com>
2014-11-27 17:36:36 +0000

    ajustes

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask,json,request
 from db.connectionfactory import ConnectionFactory
+from db.connectionfactory import db
+from model.user import User
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +14,11 @@ def hello():
 def test():
     conn = ConnectionFactory()
     return conn.create()
+
+@app.route('/<name>')
+def hello_name(name):
+    user = User.query.filter_by(first_name=name).first()
+    return jsonify(first_name = user.first_name, last_name = user.last_name, active = user.active)
 
 @app.route('/messages', methods = ['POST'])
 def api_message():
