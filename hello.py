a5f2ead2a9a64f1fe62950f844dc7b41b031fe1f
koding user <sidney.nakatani@hotmail.com>
2014-11-29 01:13:09 +0000

    ajustes

--- hello.py
+++ hello.py
@@ -1,29 +1,45 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify, redirect
+from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
 from db.connectionfactory import ConnectionFactory
 from model.user import User
 from model.post import Author
 
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'home'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
-def hello():
+def home():
     return 'Hello!'
 
 @app.route('/test')
+@login_required
 def test():
     conn = ConnectionFactory()
     return conn.create()
 
 @app.route('/<name>')
+@login_required
 def hello_name(name):
     author = Author.query.filter(Author.name == name).first()
-    return jsonify(name = author.name)
+    return redirect(jsonify(name = author.name))
 
-@app.route('/auth', methods = ['POST'])
+@app.route('/auth', methods = ['POST','GET'])
 def auth():
     email = request.form['email'] 
     password =  request.form['password']
     user = User.query.filter_by(email = email, password = password).first()
+    if user is None:
+        return 'Authorization denied'
+
+    login_user(user)
     return jsonify(auth = user.active)
 
 
