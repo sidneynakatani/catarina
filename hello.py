43c3c8bbd0a18dd1e69aa2043850829a0b4d9715
Sidney <slack.hiroshi@gmail.com>
2014-11-30 19:30:36 -0200

    Post test

--- hello.py
+++ hello.py
@@ -11,13 +11,12 @@ login_manager.init_app(app)
 login_manager.login_view = 'home'
 
 
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 @app.route('/')
 def home():
     return 'Hello!'
+
+
 
 @app.route('/test')
 @login_required
@@ -25,19 +24,25 @@ def test():
     conn = ConnectionFactory()
     return conn.create()
 
+
+
 @app.route('/<name>')
 @login_required
 def hello_name(name):
     author = Author.query.filter(Author.name == name).first()
     return redirect(jsonify(name = author.name))
 
+
+
 @app.route('/auth', methods = ['POST','GET'])
 def auth():
     
     if request.method == 'POST':
-    	email = request.form['email'] 
-    	password =  request.form['password']
-    	print 'POST Method'
+    	#email = request.form['email'] 
+    	#password =  request.form['password']
+    	email = request.form.get('email','')
+	password = request.form.get('password','')
+	print 'POST Method'
 
     if request.method == 'GET':
         email = request.args.get('email')
@@ -51,6 +56,14 @@ def auth():
 
     login_user(user)
     return jsonify(auth = user.active)
+
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 
 if __name__ == '__main__':
